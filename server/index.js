e59f816b621423bd729ade17e646fbe1266548c2
Biplab Sarkar <94435217+Biplabsarkar2018b@users.noreply.github.com>
2023-07-17 16:20:05 +0530

    localhost removed

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origins: [
-      "http://127.0.0.1:5173",
+    origin: 
+      // "http://127.0.0.1:5173",
       "https://node-react-chat-25v6.vercel.app/",
-    ],
+    
     methods: ["GET", "POST"],
   },
 });
