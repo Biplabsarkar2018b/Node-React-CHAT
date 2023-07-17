79134a0a3356ddc8e562d081951aec98670b2ba2
Biplab Sarkar <94435217+Biplabsarkar2018b@users.noreply.github.com>
2023-07-17 16:09:49 +0530

    client url updated to actual deployed server url

--- client/src/screens/ChatPage.jsx
+++ client/src/screens/ChatPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://node-react-chat.vercel.app/");
 
 const ChatPage = () => {
   const [selectedUser, setSelectedUser] = useState(null);
